f946d32b173a27012df335cd475f4f58eca406cb
David Stump <david@davidstump.net>
2015-12-17 15:59:25 -0500

    Remove page model

--- phember_backend/web/controllers/page_controller.ex
+++
@@ -1,7 +0,0 @@
-defmodule PhemberBackend.PageController do
-  use PhemberBackend.Web, :controller
-
-  def index(conn, _params) do
-    render conn, "index.html"
-  end
-end
--- phember_backend/web/views/page_view.ex
+++
@@ -1,3 +0,0 @@
-defmodule PhemberBackend.PageView do
-  use PhemberBackend.Web, :view
-end
